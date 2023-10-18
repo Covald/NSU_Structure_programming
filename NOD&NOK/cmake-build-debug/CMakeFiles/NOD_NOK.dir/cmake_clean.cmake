1c98bbc534a017f957d70cbaff320db2bed87b53
Covald <p.rezin@g.nsu.ru>
2023-10-19 04:16:05 +0700

    Initial commit

---
+++ NOD&NOK/cmake-build-debug/CMakeFiles/NOD_NOK.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NOD_NOK.dir/main.c.obj"
+  "NOD_NOK.exe"
+  "NOD_NOK.exe.manifest"
+  "NOD_NOK.lib"
+  "NOD_NOK.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NOD_NOK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ NOD&NOK/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int NOD(int a, int b){
+    while (a != b){
+        if (a > b){
+            a -= b;
+        } else {
+            b -= a;
+        }
+    }
+    return a;
+}
+
+int NOK(int a, int b){
+    return abs(a * b)/NOD(a, b);
+}
+
+int main(){
+    int a, b;
+    printf("Enter a and b: ");
+    scanf("%d %d", &a, &b);
+    printf("NOK = %d\nNOD = %d", NOK(a, b), NOD(a, b));
+    return 0;
+}
