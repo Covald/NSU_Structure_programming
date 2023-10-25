46a443455507c224552d904fa8ce9e1fe5659d84
Covald <p.rezin@g.nsu.ru>
2023-10-26 02:48:56 +0700

    fix int to long int

--- fib/main.c
+++ fib/main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned int *fib(unsigned int num) {
-    unsigned int *numbers = malloc(sizeof(unsigned int) * num);
+unsigned long int *fib(unsigned int num) {
+    unsigned int *numbers = malloc(sizeof(unsigned long int) * num);
     if (num == 1) {
         numbers[0] = 1;
     } else if (num == 2) {
         numbers[0] = 1;
         numbers[1] = 1;
     } else {
-        unsigned int a1,a2,b;
+        unsigned long int a1,a2,temp;
         a1 = 1;
         a2 = 1;
         numbers[0] = 1;
         numbers[1] = 1;
         for (int i = 2; i < num; i++) {
             numbers[i]=a1+a2;
-            b = a1;
+            temp = a1;
             a1 = a2;
-            a2 = b + a1;
+            a2 = temp + a1;
         }
     }
     return numbers;
@@ -28,9 +28,9 @@ int main() {
     printf("Enter count of Fibonacci numbers:");
     unsigned int num;
     scanf_s("%d", &num);
-    unsigned int* numbers = fib(num);
+    unsigned long int* numbers = fib(num);
     for (int i=0;i<num;i++){
-        printf("%d ",numbers[i]);
+        printf("%lu ",numbers[i]);
     }
     return 0;
 }
