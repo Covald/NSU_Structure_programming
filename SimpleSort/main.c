a30e16067569ffdcf28645c3a56bd661c5635cc5
Covald <p.rezin@g.nsu.ru>
2023-11-27 02:06:17 +0700

    add .gitignore

---
+++ SimpleSort/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+char *read_file(int size, const char *filename) {
+    FILE *fptr = fopen(filename, "r");
+    if (fptr == NULL) {
+        printf("Not able to open the input file.");
+        return "";
+    }
+    char *myString = malloc(sizeof(char) * size);
+    fgets(myString, 100, fptr);
+    fclose(fptr);
+    return myString;
+}
+
+void write_to_file(int size, const char *filename, char *string) {
+    FILE *fptr = fopen(filename, "w");
+    if (fptr == NULL) {
+        printf("Not able to open the output file.");
+        return;
+    }
+    fwprintf(fptr, string);
+    fclose(fptr);
+}
+
+int main() {
+    printf("Hello, World!\n");
+    char* string = read_file(1,"../input.txt");
+    write_to_file(1,"../output.txt",string);
+    return 0;
+}
